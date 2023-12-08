4c50f826b5dcb8dc3df882ebe587a7023c8e8fec
Roaa-Fathi03 <11410120210140@stud.cu.edu.eg>
2023-12-08 16:49:53 +0200

    Handled context time

--- SRTF.java
+++ SRTF.java
@@ -1,10 +1,6 @@
 package cpuScheduler;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-
+import java.util.*;
 public class SRTF {
     private List<Process> processes;
     private PriorityQueue<Process> readyQueue;
@@ -12,94 +8,156 @@ public class SRTF {
     private int totalWaitingTime;
     private int totalTurnaroundTime;
     private int contextTime;
-    private List<Pair<Process, Integer>> executionOrder; // New list to track execution order
+    private List<Pair<Process, Integer>> executionOrder;
+    private Set<Process> processesAddedToQueue;
 
     public SRTF(List<Process> processes, int contextTime) {
         this.processes = processes;
         this.readyQueue = new PriorityQueue<>(new ShortestRemainingTimeComparator());
-        executionOrder = new ArrayList<>();
+        this.executionOrder = new ArrayList<>();
+        this.processesAddedToQueue = new HashSet<>();
         this.contextTime = contextTime;
         this.currentTime = 0;
-        totalTurnaroundTime = 0;
-        totalWaitingTime = 0;
+        this.totalTurnaroundTime = 0;
+        this.totalWaitingTime = 0;
+    }
+
+    private void updateReadyQueue() {
+
+        for (Process process : processes) {
+            if (process.getArrivalTime() <= currentTime && !process.isCompleted() && !processesAddedToQueue.contains(process)) {
+                readyQueue.add(process);
+                processesAddedToQueue.add(process);
+            }
+        }
     }
 
     public void runScheduler() {
         int totalProcesses = processes.size();
         int completedProcesses = 0;
+        Process previousProcess = null;
 
         while (completedProcesses < totalProcesses) {
-            // Check for new arrivals and add them to the ready queue
-            for (Process process : processes) {
-                if (process.getArrivalTime() == currentTime && !process.isCompleted()) {
-                    readyQueue.add(process);
-                }
-            }
+            updateReadyQueue();
 
-            // If the ready queue is not empty, execute the process with the shortest remaining time
             if (!readyQueue.isEmpty()) {
                 Process currentProcess = readyQueue.poll();
+                currentProcess = handleContextSwitch(previousProcess, currentProcess);
 
-//                System.out.println("current is " + currentProcess.getName());
-
-                // Add the current process and the time it entered execution to the execution order
                 executionOrder.add(new Pair<>(currentProcess, currentTime));
 
-                // Execute the process for one time unit
-                currentProcess.setRemainingTime(currentProcess.getRemainingTime() - 1);
-                System.out.println("current process is " + currentProcess.getName() + " entered at time " + currentTime + " and its remaining time is " + currentProcess.getRemainingTime());
-
-                // Check if the process is completed
-                if (currentProcess.isCompleted()) {
-                    currentProcess.setTurnAroundTime(currentTime + contextTime - currentProcess.getArrivalTime() + 1);
-                    currentProcess.setWaitTime(currentProcess.getTurnAroundTime() - currentProcess.getBurstTime());
-                    System.out.println("process " + currentProcess.getName() + " has turnaround equals " + currentProcess.getTurnAroundTime() + " and waiting " + currentProcess.getWaitingTime() );
-                    totalWaitingTime += currentProcess.getWaitingTime();
-                    totalTurnaroundTime += currentProcess.getTurnAroundTime();
-                    readyQueue.remove(currentProcess);
+                int increaseCompleted = executeProcess(currentProcess);
+                if (increaseCompleted == 1){
                     completedProcesses++;
                 }
-                else {
-                    // Process is not completed, add it back to the ready queue
-                    readyQueue.add(currentProcess);
-                }
-            }
 
+                previousProcess = currentProcess;
+            }
             currentTime++;
         }
+        printResults();
+    }
 
-        // Calculate average waiting time and turnaround time
-        double averageWaitingTime = (double) totalWaitingTime / totalProcesses;
-        double averageTurnaroundTime = (double) totalTurnaroundTime / totalProcesses;
+    private Process handleContextSwitch(Process previousProcess, Process currentProcess) {
+        if (previousProcess != null && !currentProcess.getName().equals(previousProcess.getName())) {
+//            System.out.println("current process was " + currentProcess.getName());
+//            System.out.println("current time before context: " + currentTime);
+            currentTime += contextTime;
+//            System.out.println("current time after context: " + currentTime);
+
+            readyQueue.clear();
+            processesAddedToQueue.clear();
+            updateReadyQueue();
+
+            Process curr =  readyQueue.poll(); // Remove the current process as it will be added back after the context switch
+//            System.out.println("current process now is " + currentProcess.getName());
+            return curr;
+        }
+        return currentProcess;
+    }
+
+    private int executeProcess(Process currentProcess){
+        currentProcess.setRemainingTime(currentProcess.getRemainingTime() - 1);
+
+        if (currentProcess.isCompleted()) {
+            calculateTurnaround(currentProcess);
+            calculateWaitingTime(currentProcess);
+
+//            System.out.println("the ended process turnaround is " + currentProcess.getTurnAroundTime());
+//            System.out.println("the ended process waiting is " + currentProcess.getWaitingTime());
+
+            totalWaitingTime += currentProcess.getWaitingTime();
+            totalTurnaroundTime += currentProcess.getTurnAroundTime();
+
+//            processes.removeIf(Process::isCompleted);
+            readyQueue.removeIf(Process::isCompleted);
+            processesAddedToQueue.removeIf(Process::isCompleted);
+            return 1;
+        }
+        else {
+            readyQueue.add(currentProcess);
+        }
+        return 0;
+    }
+
+    private void calculateTurnaround(Process process) {
+        process.setTurnAroundTime(currentTime - process.getArrivalTime() + 1 + contextTime);
+    }
+
+    private void calculateWaitingTime(Process process) {
+        process.setWaitTime(process.getTurnAroundTime() - process.getBurstTime());
+    }
+
+    private void printResults() {
+        double averageWaitingTime = (double) totalWaitingTime / processes.size();
+        double averageTurnaroundTime = (double) totalTurnaroundTime / processes.size();
 
         System.out.println("Average Waiting Time: " + averageWaitingTime);
         System.out.println("Average Turnaround Time: " + averageTurnaroundTime);
 
-        // Print the execution order
-//        System.out.println("Execution Order:");
-//        for (Pair<Process, Integer> pair : executionOrder) {
-//            System.out.println("Process " + pair.getFirst().getName() +
-//                    " entered execution at time " + pair.getSecond());
-//        }
+        System.out.println("Execution Order:");
+        for (Pair<Process, Integer> pair : executionOrder) {
+            System.out.println("Process " + pair.getFirst().getName() +
+                    " entered execution at time " + pair.getSecond());
+        }
+    }
+
+    public void printPro(List<Process> processes) {
+        for (Process p : processes){
+            System.out.print(p + " ");
+        }
+        System.out.println(" ");
+    }
+
+    public void print(Set<Process> set) {
+        for (Process s : set){
+            System.out.print(s + " ");
+        }
+        System.out.println(" ");
+    }
+    public void printReady(PriorityQueue<Process> readyQueue) {
+        for (Process r : readyQueue){
+            System.out.print(r + " ");
+        }
+        System.out.println(" ");
+
     }
 
     public static void main(String[] args) {
-        // Example usage
         List<Process> processes = new ArrayList<>();
         processes.add(new Process("P1", "Red", 2, 6, 3));
         processes.add(new Process("P2", "Blue", 5, 2, 1));
         processes.add(new Process("P3", "Green", 1, 8, 7));
-        processes.add(new Process("P4", "Green", 0, 3, 8));
-        processes.add(new Process("P5", "Green", 4, 4, 2));
+        processes.add(new Process("P4", "purple", 0, 3, 8));
+        processes.add(new Process("P5", "orange", 4, 4, 2));
 
-        int contextTime = 0;
+        int contextTime = 1;
 
         SRTF srtfScheduler = new SRTF(processes, contextTime);
         srtfScheduler.runScheduler();
     }
 }
 
-// Define Pair class to store pairs
 class Pair<F, S> {
     private final F first;
     private final S second;
@@ -118,10 +176,15 @@ class Pair<F, S> {
     }
 }
 
-
 class ShortestRemainingTimeComparator implements Comparator<Process> {
     @Override
     public int compare(Process p1, Process p2) {
-        return Integer.compare(p1.getRemainingTime(), p2.getRemainingTime());
+        int result = Integer.compare(p1.getRemainingTime(), p2.getRemainingTime());
+
+        if (result == 0) {
+            result = Integer.compare(p1.getArrivalTime(), p2.getArrivalTime());
+        }
+
+        return result;
     }
 }
